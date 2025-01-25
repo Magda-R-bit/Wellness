46bb00697be0732c2835b5b2f3efd378c084c7de
Magdalena Redka <magda.redka@gmail.com>
2025-01-25 15:32:14 +0000

    Create bookings model

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Bookings
 
 # Register your models here.
+
+@admin.register(Bookings)
+class BookingsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'item_name', 'check_in', 'check_out', 'total_price', 'created_at')
+    list_filter = ('check_in', 'check_out', 'created_at')
+    search_fields = ('user_username', 'item_name')
