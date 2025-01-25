116ca8105d528cfae280a3886c58c9df76429392
Magdalena Redka <magda.redka@gmail.com>
2025-01-25 16:18:27 +0000

    Add urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import Index
+from . import views
 
 urlpatterns = [
     path('', Index.as_view(), name='bookings'),
+    path('', views.booking_list, name='booking_list'),
+    path('create/', views.create_booking, name='create_booking'),
+    path('<int:pk>/', views.booking_detail, name='booking_detail'),
    
 ]
